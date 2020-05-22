6260fec7fd765b5871f0dac76bb8c07534ac078f
Graham Leva <celaxodon@gmail.com>
2020-05-22 09:44:27 -0500

    WIP add platform-t210-porg DTS package

--- package/hardware-nvidia/soc-t210/soc_t210.mk
+++ package/hardware-nvidia/soc-t210/soc_t210.mk
@@ -8,12 +8,12 @@ HARDWARE_NVIDIA_SOC_T210_LICENSE = GPL-2.0
 HARDWARE_NVIDIA_SOC_T210_VERSION = tegra-l4t-r32.4.2
 HARDWARE_NVIDIA_SOC_T210_SITE = git://nv-tegra.nvidia.com/device/hardware/nvidia/soc/t210.git
 HARDWARE_NVIDIA_SOC_T210_INSTALL_STAGING = YES
-HARDWARE_NVIDIA_SOC_T210__DL_DIR = $(BUILD_DIR)
+HARDWARE_NVIDIA_SOC_T210_DL_DIR = $(BUILD_DIR)
 
 define HARDWARE_NVIDIA_SOC_T210_CONFIGURE_CMDS
-    mkdir -p $(BUILD_DIR)/hardware-nvidia/soc
+    mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia
 	ln -s $(BUILD_DIR)/hardware-nvidia-soc-t210-$(HARDWARE_NVIDIA_SOC_T210_VERSION) \
-		$(BUILD_DIR)/hardware-nvidia/soc/t210
+		$(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/soc/t210/soc
 endef
 
 $(eval $(generic-package))
