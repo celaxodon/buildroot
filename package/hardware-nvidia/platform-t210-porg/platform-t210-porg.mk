8e52b9f611bc973ba33cdade54578d1fc6f47245
Graham Leva <celaxodon@gmail.com>
2020-05-22 09:44:27 -0500

    WIP add platform-t210-porg DTS package

---
+++ package/hardware-nvidia/platform-t210-porg/platform-t210-porg.mk
@@ -0,0 +1,18 @@
+#################################################
+#
+# Platform T210 Porg download helper
+#
+#################################################
+
+PLATFORM_T210_PORG_LICENSE = GPL-2.0
+PLATFORM_T210_PORG_VERSION = tegra-l4t-r32.4.2
+PLATFORM_T210_PORG_SITE = git://nv-tegra.nvidia.com/device/hardware/nvidia/platform/t210/porg.git
+PLATFORM_T210_PORG_INSTALL_STAGING = YES
+
+define PLATFORM_T210_PORG_CONFIGURE_CMDS
+    mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/platform/t210
+	ln -s $(BUILD_DIR)/hardware-nvidia-platform-t210-porg-$(PLATFORM_T210_PORG_VERSION) \
+		$(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/platform/t210/porg
+endef
+
+$(eval $(generic-package))
