06c732efc7e9257ec023b7d177ebcdd49866561e
Graham Leva <celaxodon@gmail.com>
2020-06-10 22:59:09 -0500

    WIP: Add and refine nvidia dts tree configs

---
+++ package/hardware-nvidia/soc-t19x/soc-t19x.mk
@@ -0,0 +1,18 @@
+##############################################################################
+#
+# SOC t19x
+#
+##############################################################################
+
+SOC_T19X_LICENSE = GPL-2.0
+SOC_T19X_VERSION = tegra-l4t-r32.4.2
+SOC_T19X_SITE = git://nv-tegra.nvidia.com/device/hardware/nvidia/soc/t19x.git
+SOC_T19X_INSTALL_STAGING = YES
+
+define SOC_T19X_CONFIGURE_CMDS
+    mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/soc
+	ln -s $(BUILD_DIR)/soc-t19x-$(SOC_T19X_VERSION) \
+		$(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/soc/t19x
+endef
+
+$(eval $(generic-package))
