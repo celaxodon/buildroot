f3554fd570152a2fc2412db24de47c8acfa48634
Graham Leva <celaxodon@gmail.com>
2020-06-18 16:27:52 -0500

    WIP: First successful build of L4T kernel with DTS

--- package/hardware-nvidia/platform-t210-common/platform-t210-common.mk
+++ package/hardware-nvidia/platform-t210-common/platform-t210-common.mk
@@ -10,9 +10,9 @@ PLATFORM_T210_COMMON_SITE = git://nv-tegra.nvidia.com/device/hardware/nvidia/pla
 PLATFORM_T210_COMMON_INSTALL_STAGING = YES
 
 define PLATFORM_T210_COMMON_CONFIGURE_CMDS
-    mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/platform/t210
+    mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/platform/t210
 	ln -s $(BUILD_DIR)/platform-t210-common-$(PLATFORM_T210_COMMON_VERSION) \
-		$(NV_BUILD_KERNEL_DTS_ROOT)/hardware/nvidia/platform/t210/common
+		$(NV_BUILD_KERNEL_DTS_ROOT)/platform/t210/common
 endef
 
 $(eval $(generic-package))
