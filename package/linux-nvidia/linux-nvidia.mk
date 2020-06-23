55f85b95dd8ab327e1c82486071b7d1212b581ee
Graham Leva <celaxodon@gmail.com>
2020-06-23 11:18:24 -0500

    WIP: Export NV_BUILD_KERNEL_DTS_BUILD in linux-nvidia package

--- package/linux-nvidia/linux-nvidia.mk
+++ package/linux-nvidia/linux-nvidia.mk
@@ -9,6 +9,9 @@ LINUX_NVIDIA_LICENSE = GPL-2.0
 LINUX_NVIDIA_VERSION = tegra-l4t-r32.4.2
 LINUX_NVIDIA_SITE = git://nv-tegra.nvidia.com/linux-nvidia.git
 
+# Must be defined here as well as in hardware-nvidia packages
+NV_BUILD_KERNEL_DTS_ROOT=$(BUILD_DIR)/hardware/nvidia
+
 define LINUX_NVIDIA_CONFIGURE_CMDS
 	ln -s $(BUILD_DIR)/linux-nvidia-$(LINUX_NVIDIA_VERSION) $(BUILD_DIR)/nvidia
 endef
