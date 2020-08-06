27f543bd063b0eb6270ee84a33d1875f1fcd60b8
Graham Leva <celaxodon@gmail.com>
2020-08-05 23:34:15 -0500

    WIP: Lint added nvidia packages

--- package/hardware-nvidia/soc-t210/soc-t210.mk
+++ package/hardware-nvidia/soc-t210/soc-t210.mk
@@ -1,8 +1,8 @@
-##############################################################################
+################################################################################
 #
 # SOC t210
 #
-##############################################################################
+################################################################################
 
 SOC_T210_LICENSE = GPL-2.0
 SOC_T210_VERSION = tegra-l4t-r32.4.2
@@ -10,7 +10,7 @@ SOC_T210_SITE = git://nv-tegra.nvidia.com/device/hardware/nvidia/soc/t210.git
 SOC_T210_INSTALL_STAGING = YES
 
 define SOC_T210_CONFIGURE_CMDS
-    mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/soc
+	mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/soc
 	ln -s $(BUILD_DIR)/soc-t210-$(SOC_T210_VERSION) \
 		$(NV_BUILD_KERNEL_DTS_ROOT)/soc/t210
 endef
