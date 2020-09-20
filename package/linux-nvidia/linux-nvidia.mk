7522e491acb2f49791f49b4bf7c70fc7377668ef
Graham Leva <celaxodon@gmail.com>
2020-09-19 19:49:09 -0500

    WIP: specify custom toolchain with GCC version

--- package/linux-nvidia/linux-nvidia.mk
+++ package/linux-nvidia/linux-nvidia.mk
@@ -7,11 +7,10 @@
 LINUX_NVIDIA_VERSION = tegra-l4t-r32.4.2
 LINUX_NVIDIA_SITE = git://nv-tegra.nvidia.com/linux-nvidia.git
 LINUX_NVIDIA_LICENSE = GPL-2.0
-LINUX_NVIDIA_LICENSE_FILES = TODO
+# LINUX_NVIDIA_LICENSE_FILES = TODO
 
 # Must be defined here as well as in hardware-nvidia packages
-NV_BUILD_KERNEL_DTS_ROOT=$(BUILD_DIR)/hardware/nvidia
-export NV_BUILD_KERNEL_DTS_ROOT
+export NV_BUILD_KERNEL_DTS_ROOT=$(BUILD_DIR)/hardware/nvidia
 
 define LINUX_NVIDIA_CONFIGURE_CMDS
 	ln -s $(BUILD_DIR)/linux-nvidia-$(LINUX_NVIDIA_VERSION) $(BUILD_DIR)/nvidia
