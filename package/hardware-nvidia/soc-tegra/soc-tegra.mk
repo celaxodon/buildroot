321922e83f266ae2cdb3bf5ec891fb272af8e492
Graham Leva <celaxodon@gmail.com>
2020-11-04 07:27:59 -0600

    WIP: Make package structure and order more consistent

--- package/hardware-nvidia/soc-tegra/soc-tegra.mk
+++ package/hardware-nvidia/soc-tegra/soc-tegra.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SOC_TEGRA_LICENSE = GPL-2.0
-SOC_TEGRA_LICENSE_FILES = kernel-include/dt-bindings/media/tegra-vi.h
 SOC_TEGRA_VERSION = tegra-l4t-r32.4.2
 SOC_TEGRA_SITE = git://nv-tegra.nvidia.com/device/hardware/nvidia/soc/tegra.git
+SOC_TEGRA_LICENSE = GPL-2.0
+SOC_TEGRA_LICENSE_FILES = kernel-include/dt-bindings/media/tegra-vi.h
 
 define SOC_TEGRA_CONFIGURE_CMDS
 	mkdir -p $(NV_BUILD_KERNEL_DTS_ROOT)/soc
--- package/linux-nvidia/linux-nvidia.mk
+++ package/linux-nvidia/linux-nvidia.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-LINUX_NVIDIA_LICENSE = GPL-2.0
-LINUX_NVIDIA_LICENSE_FILES = drivers/Makefile
 LINUX_NVIDIA_VERSION = tegra-l4t-r32.4.2
 LINUX_NVIDIA_SITE = git://nv-tegra.nvidia.com/linux-nvidia.git
+LINUX_NVIDIA_LICENSE = GPL-2.0
+LINUX_NVIDIA_LICENSE_FILES = drivers/Makefile
 
 # Must be defined here as well as in hardware-nvidia packages
 export NV_BUILD_KERNEL_DTS_ROOT=$(BUILD_DIR)/hardware/nvidia
 
+# L4T build process requires specific directory layouts
 define LINUX_NVIDIA_CONFIGURE_CMDS
 	ln -s $(BUILD_DIR)/linux-nvidia-$(LINUX_NVIDIA_VERSION) $(BUILD_DIR)/nvidia
 endef
